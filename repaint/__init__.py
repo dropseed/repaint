1645c86fa485a62a75586448aa297fef76a9c643
Dave Gaeddert <dave.gaeddert@gmail.com>
2022-07-01 15:41:29 -0500

    Add Repaint to init

--- repaint/__init__.py
+++ repaint/__init__.py
@@ -1,18 +1,3 @@
-import asyncio
+from .core import Repaint
 
-import websockets
-
-
-def reload():
-    async def hello():
-        uri = "ws://localhost:8765"
-        async with websockets.connect(uri) as websocket:
-            # name = input("What's your name? ")
-
-            await websocket.send("reload")
-            print(f"> reload send")
-
-            # greeting = await websocket.recv()
-            # print(f"< {greeting}")
-
-    asyncio.get_event_loop().run_until_complete(hello())
+__all__ = ["Repaint"]
